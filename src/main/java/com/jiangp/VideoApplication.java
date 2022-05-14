253e5b1749e0e35ee12e3d81885df07a548f9fa0
jiangpeng <asdasdassd>
2022-05-14 17:33:12 +0800

    优化部分代码

--- src/main/java/com/jiangp/VideoApplication.java
+++ src/main/java/com/jiangp/VideoApplication.java
@@ -1,13 +1,29 @@
 package com.jiangp;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @SpringBootApplication
+@Slf4j
 public class VideoApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(VideoApplication.class, args);
+    public static void main(String[] args) throws UnknownHostException {
+        ConfigurableApplicationContext application = SpringApplication.run(VideoApplication.class, args);
+
+        Environment env = application.getEnvironment();
+        String ip = InetAddress.getLocalHost().getHostAddress();
+        String port = env.getProperty("server.port");
+        log.info("\n----------------------------------------------------------\n\t" +
+                "Application Candid-Photo is running! Access URLs:\n\t" +
+                "Local: \t\thttp://localhost:" + port +"/\n\t" +
+                "Local: \t\thttp://" + ip + ":" + port + "/\n\t" +
+                "----------------------------------------------------------");
     }
 
 }
--- src/main/java/com/jiangp/controller/MainController.java
+++ src/main/java/com/jiangp/controller/MainController.java
@@ -1,12 +1,18 @@
 package com.jiangp.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.annotation.PostConstruct;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.text.SimpleDateFormat;
@@ -14,40 +20,55 @@ import java.util.Base64;
 import java.util.Date;
 
 @Controller
+@Slf4j
 public class MainController {
 
-    private static String imagePath = "C:\\Users\\jiang\\Desktop\\";
-//    private static String imagePath = "/root/img/";
+    @Value("${image.url}")
+    private String imagePath;
+    @Value("${image.suffix}")
+    private String suffix;
 
+    private Path getPath(){
+        String path = imagePath + System.currentTimeMillis() + suffix;
+        log.info("========>>>>>>>>  存储文件路径：{}", path);
+        return Paths.get(path);
+    }
 
-    @RequestMapping(value = {"/","/index"})
-    public String index(){
+    @RequestMapping(value = {"/", "/index"})
+    public String index() {
         return "index";
     }
 
     /**
      * base64字符串上传
+     *
      * @param photo
      */
-    @RequestMapping(value = "/base64",method = RequestMethod.GET)
+    @RequestMapping(value = "/base64", method = RequestMethod.GET)
     @ResponseBody
-    public void base64(String photo){
-        GenerateImage(photo);
+    public void base64(String photo) throws Exception {
+        if (photo == null) {
+            log.error("生成文件失败，请给出相应的数据。");
+            return;
+        }
+        byte[] decode = Base64.getMimeDecoder().decode(photo);
+        Files.write(getPath(), decode, StandardOpenOption.CREATE);
+
+        log.info("指定路径下生成文件成功。");
     }
 
-
-    //base64转图片并储存
-    public static String GenerateImage(String base64) {
-        if (base64 == null) {
-            return "生成文件失败，请给出相应的数据。";
+    /**
+     * MultipartFile上传
+     * @param photo
+     */
+    @ResponseBody
+    @PostMapping(value = "/upload", headers = "content-type=multipart/form-data")
+    public void upload(MultipartFile photo) throws Exception {
+        if(photo.isEmpty()){
+            log.error("生成文件失败，请给出相应的数据。");
+            return;
         }
-        try {
-            //设置日期格式
-            SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd-HH：mm：ss");
-            Files.write(Paths.get(imagePath+ df.format(new Date())+".jpg"), Base64.getMimeDecoder().decode(base64), StandardOpenOption.CREATE);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return "指定路径下生成文件成功！";
+        Files.write(getPath(), photo.getBytes(), StandardOpenOption.CREATE);
+        log.info("指定路径下生成文件成功。");
     }
 }
